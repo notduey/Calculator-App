8c5d0a4e8a6ae706687d8178c783934b8a7b7ec0
notduey <notduey@gmail.com>
2025-07-09 17:52:55 -0700

    Coded the display label/panel/text in Calculator class, also added comments for clarity.

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -5,19 +5,35 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class Calculator {
-    int windowWidth = 360;
-    int windowHeight = 540;
+    int windowWidth = 360; //width of window
+    int windowHeight = 540; //height of window
 
-    JFrame frame =  new JFrame("Calculator");
+    Color customLightGray = new Color(212, 212, 210); //custom gray color
+    Color customDarkGray = new Color(80, 80, 80); //custom dark gray
+    Color customBlack = new Color(28, 28, 28); //custom black
+    Color customPurple = new Color(143, 78, 255); //custom purple
 
-    Calculator() {
-        frame.setVisible(true);
-        frame.setSize(windowWidth, windowHeight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+    JFrame frame =  new JFrame("Calculator"); //the window itself (JFrame) with calculator as the name
+    JLabel displayLabel = new JLabel(); //label for the text
+    JPanel displayPanel = new JPanel(); //panel for the label
 
+    Calculator() { //constructor class for window properties
+        frame.setVisible(true); //true so the window is visible
+        frame.setSize(windowWidth, windowHeight); //frame size is same as window width and height
+        frame.setLocationRelativeTo(null); //setting location to null centers the window when the user opens the application
+        frame.setResizable(false); //false makes it so the user cannot resize the window in any way
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes it so when the user clicks on the x button the program will be terminated (closed)
+        frame.setLayout(new BorderLayout()); //BorderLayout makes it so you can place stuff within the window
+
+        displayLabel.setBackground(customBlack); //label background color
+        displayLabel.setForeground(Color.white); //text (foreground) color
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80)); //font for the label; font, formatting, size
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT); //offsets text to the right
+        displayLabel.setText("0"); //sets default text
+        displayLabel.setOpaque(true); //true makes label opaque
+
+        displayPanel.setLayout(new BorderLayout()); //setting the layout
+        displayPanel.add(displayLabel); //putting the text label inside the panel
+        frame.add(displayPanel); //putting the panel in our window
     }
-
 }
